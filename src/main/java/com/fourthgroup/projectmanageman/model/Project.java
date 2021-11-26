4b67a33be033fd9cd1332e0e6b2b76560c501df1
FrederikWvB <frederik.xiao@gmail.com>
2021-11-26 14:08:15 +0100

    populate Project and UserProjectRole classes

--- src/main/java/com/fourthgroup/projectmanageman/model/Project.java
+++ src/main/java/com/fourthgroup/projectmanageman/model/Project.java
@@ -1,4 +1,118 @@
 package com.fourthgroup.projectmanageman.model;
 
+import java.time.LocalDateTime;
+
+/*
+    ===============================
+    Author: Frederik Wandall von Benzon
+    Date: Nov 26, 2021
+    ===============================
+ */
+
 public class Project {
+    int id;
+    int parentProjectID;
+    String title;
+    String client;
+    LocalDateTime startDate; //Java: 2021-11-28T10:15:30 - SQL format: YYYY-MM-DD
+    LocalDateTime deadline; //Java: 2021-11-28T10:15:30 - SQL format: YYYY-MM-DD
+    int estTimeHours;
+    int spentTimeHours;
+    Status status;
+
+    public Project(int id, int parentProjectID, String title, String client, LocalDateTime startDate, LocalDateTime deadline, int estTimeHours, int spentTimeHours, Status status) {
+        this.id = id;
+        this.parentProjectID = parentProjectID;
+        this.title = title;
+        this.client = client;
+        this.startDate = startDate;
+        this.deadline = deadline;
+        this.estTimeHours = estTimeHours;
+        this.spentTimeHours = spentTimeHours;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", parentProjectID=" + parentProjectID +
+                ", title='" + title + '\'' +
+                ", client='" + client + '\'' +
+                ", startDate=" + startDate +
+                ", deadline=" + deadline +
+                ", estTimeHours=" + estTimeHours +
+                ", spentTimeHours=" + spentTimeHours +
+                ", status=" + status +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getParentProjectID() {
+        return parentProjectID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public int getEstTimeHours() {
+        return estTimeHours;
+    }
+
+    public int getSpentTimeHours() {
+        return spentTimeHours;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setParentProjectID(int parentProjectID) {
+        this.parentProjectID = parentProjectID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setEstTimeHours(int estTimeHours) {
+        this.estTimeHours = estTimeHours;
+    }
+
+    public void setSpentTimeHours(int spentTimeHours) {
+        this.spentTimeHours = spentTimeHours;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
+
--- src/main/java/com/fourthgroup/projectmanageman/model/UserProjectRole.java
+++ src/main/java/com/fourthgroup/projectmanageman/model/UserProjectRole.java
@@ -1,4 +1,67 @@
 package com.fourthgroup.projectmanageman.model;
 
+/*
+    ===============================
+    Author: Frederik Wandall von Benzon
+    Date: Nov 26, 2021
+    ===============================
+ */
+
+
 public class UserProjectRole {
+    int id;
+    int roleId;
+    int personId;
+    int projectId;
+
+    public UserProjectRole(int id, int roleId, int personId, int projectId) {
+        this.id = id;
+        this.roleId = roleId;
+        this.personId = personId;
+        this.projectId = projectId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProjectRole{" +
+                "id=" + id +
+                ", roleId=" + roleId +
+                ", personId=" + personId +
+                ", projectId=" + projectId +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    /*
+    public void setId(int id) {
+        this.id = id;
+    }
+    */
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
 }
