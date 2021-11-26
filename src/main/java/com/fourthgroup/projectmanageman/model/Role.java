c57708dc17b402fd31838d37e94c7f3ab6497327
Mark Hansen <Mark024e@stud.kea.dk>
2021-11-26 13:42:33 +0100

    attributes and constructers written for User and Role

--- src/main/java/com/fourthgroup/projectmanageman/model/Role.java
+++ src/main/java/com/fourthgroup/projectmanageman/model/Role.java
@@ -1,4 +1,41 @@
 package com.fourthgroup.projectmanageman.model;
 
+//@Mark Kaplan Hansen
 public class Role {
+    private int id;
+    private String roleName;
+    private String roleDescription;
+
+    public Role() {
+    }
+
+    public Role(int id, String roleName, String roleDescription) {
+        this.id = id;
+        this.roleName = roleName;
+        this.roleDescription = roleDescription;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
 }
