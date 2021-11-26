be77adbfee45d6a227565b3f31de8f165d97936c
Simon Roswall <simonroswall@gmail.com>
2021-11-26 13:58:38 +0100

    populate task.java

--- src/main/java/com/fourthgroup/projectmanageman/model/Assignment.java
+++ src/main/java/com/fourthgroup/projectmanageman/model/Assignment.java
@@ -1,4 +1,35 @@
 package com.fourthgroup.projectmanageman.model;
 
 public class Assignment {
+
+    private final int id;
+    private final UserProjectRole role;
+    private final int taskID;
+
+    public Assignment(int id, UserProjectRole role, int taskID){
+        this.id = id;
+        this.role = role;
+        this.taskID = taskID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public UserProjectRole getRole() {
+        return role;
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "id=" + id +
+                ", role=" + role +
+                ", taskID=" + taskID +
+                '}';
+    }
 }
