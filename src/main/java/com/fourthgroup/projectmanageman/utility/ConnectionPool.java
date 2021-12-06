10fa0d64d2df9c62cb05bc48d7cf646727b34e57
Simon Roswall <simonroswall@gmail.com>
2021-12-06 10:06:07 +0100

    refactored db connectionpool to account for our change to JawsDB

--- src/main/java/com/fourthgroup/projectmanageman/utility/ConnectionPool.java
+++ src/main/java/com/fourthgroup/projectmanageman/utility/ConnectionPool.java
@@ -6,7 +6,4 @@ public interface ConnectionPool {
 
     Connection getConnection();
     boolean releaseConnection(Connection connection);
-    String getUrl();
-    String getUsername();
-    String getPassword();
 }
--- src/main/java/com/fourthgroup/projectmanageman/utility/MySQLConnectionPool.java
+++ src/main/java/com/fourthgroup/projectmanageman/utility/MySQLConnectionPool.java
@@ -1,12 +1,16 @@
 package com.fourthgroup.projectmanageman.utility;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.*;
 import java.util.*;
 
 /*
     ===============================
     Author: Simon Roswall Jørgensen
-    Date: Nov 26, 2021
+    Original creation Date: Nov 27, 2021
+    Last update: Dec 3, 2021
+    Notes: Changed provider to JawsDB, refactored class to connect using URI/URL only.
     ===============================
 
     Source: https://www.baeldung.com/java-connection-pooling
@@ -14,64 +18,107 @@ import java.util.*;
 
 public class MySQLConnectionPool implements ConnectionPool {
 
-    private String url;
-    private String username;
-    private String password;
-    private List<Connection> pool;
-    private List<Connection> usedConnections = new ArrayList<>();
-    private static int INITIAL_POOL_SIZE = 10;
+    private final String url;
+    private final String username;
+    private final String password;
 
-    public static MySQLConnectionPool create(
-            String url, String username, String password)
-            throws SQLException {
+    private final List<Connection> availableConnections;
+    private final List<Connection> usedConnections = new ArrayList<>();
+    private final static int INITIAL_POOL_SIZE = 5;
+    private static int CURRENT_POOL_SIZE = 5;
+    private final static int MAXIMUM_POOL_SIZE = 10;
 
-        List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+    public static MySQLConnectionPool create(String URL)
+            throws URISyntaxException, SQLException {
+
+        URI jdbUri = new URI(URL);
+        String username = jdbUri.getUserInfo().split(":")[0];
+        String password = jdbUri.getUserInfo().split(":")[1];
+        String port = String.valueOf(jdbUri.getPort());
+        String jdbUrl = "jdbc:mysql://" +jdbUri.getHost() + ":" + port + jdbUri.getPath();
+
+        List<Connection> availableConnections = new ArrayList<>(INITIAL_POOL_SIZE);
+
         for(int i = 0; i < INITIAL_POOL_SIZE; i++){
-            pool.add(createConnection(url, username, password));
+            availableConnections.add(createConnection(jdbUrl, username, password));
         }
 
-        return new MySQLConnectionPool(url, username, password, pool);
+        return new MySQLConnectionPool(jdbUrl, username, password, availableConnections);
     }
 
-    private MySQLConnectionPool(String url, String username, String password, List<Connection> pool){
+    private MySQLConnectionPool(String url, String username, String password,
+                                List<Connection> availableConnections){
         this.url = url;
         this.username = username;
         this.password = password;
-        this.pool = pool;
+        this.availableConnections = availableConnections;
     }
 
     @Override
     public Connection getConnection() {
-        Connection connection = pool.remove(pool.size()-1);
-        usedConnections.add(connection);
+        Connection connection = null;
+        if (availableConnections.size() == 0 && CURRENT_POOL_SIZE < MAXIMUM_POOL_SIZE){
+            try {
+                connection = addConnection();
+            } catch (SQLException err) {
+                System.out.println("Attempted to increase connection pool, " +
+                        "something went wrong: " + err);
+            }
+        } else {
+            connection = availableConnections.remove(availableConnections.size() - 1);
+            try {
+                if (!connection.isValid(1)){
+                    connection = addConnection();
+                }
+            } catch (SQLException err) {
+                    System.out.println("Connection was inactive, something went wrong" +
+                            " when a substitute was attempted: " + err);
+                }
+        } if (connection != null) {
+            usedConnections.add(connection);
+        }
         return connection;
     }
 
     @Override
     public boolean releaseConnection(Connection connection) {
-        pool.add(connection);
-        return usedConnections.remove(connection);
+        boolean status = false;
+        if (CURRENT_POOL_SIZE > INITIAL_POOL_SIZE && availableConnections.size() > 1){
+            try {
+                status = usedConnections.remove(connection);
+                dropConnection(connection);
+            } catch (SQLException err){
+                System.out.println("Tried closing extraneous connection, " +
+                        "however something went wrong: " + err);
+            }
+        } else {
+            availableConnections.add(connection);
+            status = usedConnections.remove(connection);
+        }
+        return status;
+    }
+
+    public int getAvailableConnectionsCount(){
+        return this.availableConnections.size();
+    }
+
+    public int getUsedConnectionsCount(){
+        return this.usedConnections.size();
+    }
+
+    private Connection addConnection() throws SQLException{
+        CURRENT_POOL_SIZE++;
+        return createConnection(url, username,password);
+    }
+
+    private void dropConnection(Connection connection) throws SQLException{
+        connection.close();
+        CURRENT_POOL_SIZE--;
     }
 
     private static Connection createConnection(
             String url, String username, String password)
             throws SQLException {
         return DriverManager.getConnection(url, username, password);
-    }
-
-
-    @Override
-    public String getUrl() {
-        return url;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
     }
 }
