c614b807779077874a86320edf592a9f32569595
Roswall <simonroswall@gmail.com>
2021-12-16 19:19:18 +0100

    remove unused imports from task controller

--- src/main/java/com/fourthgroup/projectmanageman/controller/TaskController.java
+++ src/main/java/com/fourthgroup/projectmanageman/controller/TaskController.java
@@ -8,12 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
-import java.util.Set;
 
 /*
     ===============================
